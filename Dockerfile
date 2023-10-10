bcb9239dddc925932b6471ba9a3e584e7ddc2ecf
Ubuntu <ubuntu@ip-172-31-3-74.ap-south-1.compute.internal>
2023-10-10 14:14:29 +0000

    added few files

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,25 @@
-FROM python:3.10-slim-buster
+FROM nvidia/cuda:12.2.0-base-ubuntu22.04 AS builder
 
-WORKDIR /app
-RUN apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+WORKDIR /workspace
 
-RUN curl --proto '=https' --tlsv1.2 -sSf -y https://sh.rustup.rs | sh
-ENV PATH /home/pn/.cargo/bin:$PATH
+RUN apt-get update && apt-get install -y \
+    python3 \
+    python3-pip \
+    python3-dev
 
-COPY requirements.txt requirements.txt
+COPY requirements.txt /workspace/requirements.txt
 
-RUN pip install torch torchvision --index-url https://download.pytorch.org/whl/cpu
-RUN pip install timm==0.9.7
-RUN pip install -r requirements.txt
+RUN pip3 install --upgrade pip
+RUN pip3 install torch torchvision
+RUN pip3 install timm
+RUN pip3 install -r requirements.txt
+
+RUN apt install uvicorn -y
+RUN apt-get update && apt-get install -y libglib2.0-0 libsm6 libxrender1 libxext6
+RUN apt-get install ffmpeg libsm6 libxext6  -y
 
 COPY . .
 
 EXPOSE 8000
 
 CMD ["uvicorn", "server:app", "--host", "0.0.0.0", "--port", "8000"]
-
--- server.py
+++ server.py
@@ -2,30 +2,52 @@ from fastapi import FastAPI, UploadFile
 from ultralytics import YOLO
 from PIL import Image
 from typing import List
+import torch
 from datetime import datetime
+import os
+# os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 
-device = "cpu"
-model = YOLO('yolov8m.pt')
-model = model.to(device = device)
+device = "cuda"
+model = YOLO('yolov8s.pt')
+model = model.to(device=device)
+
+# Initialize a counter variable
+run_counter = 0
 
 def personDetect(image):
-  image = image.convert("RGB")
-  output = model(image, classes=[0])
-  time = datetime.now().strftime("%H%M%S.%f")
-  if len(output[0].boxes.cls) > 0:
-    print("{0} Person exists {1}".format(time, True))
-    return True
-  print("{0} Person exists {1}".format(time, False))
-  return False
+    # image = image.convert("RGB")
+    # output = model(image, classes=[0])
+    with torch.no_grad():
+        outputs = model(image, classes=[0])
+    result = []
+    for output in outputs:
+        if len(output.boxes.cls) > 0:
+            result.append(True)
+        else:
+            result.append(False)
+
+    return result
 
 app = FastAPI()
 
-
 @app.post("/yolo-speed-check")
 def yolo_speed_check(images: List[UploadFile]):
+    global run_counter  # Access the counter variable
+    batch = []
     for image in images:
-        img = Image.open(image.file)
-        person_exist = personDetect(img)
-        return {"is_person_exist": person_exist}
-    return {"is_person_exist": False}
+        with Image.open(image.file) as img:
+            batch.append(img.convert("RGB"))
+        image.file.close()
+    
+    person_exist_list = personDetect(batch)
 
+    # Increment the counter
+    run_counter += 1
+
+    # Check if the counter has reached 100, and clear CUDA cache if so
+    print(run_counter)
+    if run_counter >= 100:
+        torch.cuda.empty_cache()
+        run_counter = 0  # Reset the counter
+
+    return person_exist_list
